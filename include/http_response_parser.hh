675690551badf345b494112469c94b7678e52ae0
Erik Johnston <erikj@jki.re>
2014-12-20 13:18:22 +0000

    Implement all the httpp11 callbacks

--- include/http_response_parser.hh
+++ include/http_response_parser.hh
@@ -5,24 +5,50 @@
 #include "sink.hh"
 
 #include <cstdint>
+#include <functional>
+#include <map>
+#include <vector>
+
+
+namespace {
+    struct ci_less : std::binary_function<std::string, std::string, bool> {
+        struct nocase_compare : public std::binary_function<unsigned char, unsigned char, bool> {
+            bool operator() (const unsigned char& c1, const unsigned char& c2) const {
+                return tolower (c1) < tolower (c2);
+            }
+        };
+
+        bool operator() (const std::string & s1, const std::string & s2) const {
+            return std::lexicographical_compare(
+                    s1.begin (), s1.end (),
+                    s2.begin (), s2.end (),
+                    nocase_compare ()
+            );
+        }
+    };
+}
+
+
+using HttpHeaders = std::multimap<std::string, std::vector<char>, ci_less>;
+
 
 struct HttpVersion : public std::tuple<std::uint8_t, std::uint8_t> {
-    using std::tuple::tuple;
+    using tuple::tuple;
 
-    std::unit8_t const& major() const { return std::get<0>(*this); }
-    std::unit8_t & major() { return std::get<0>(*this); }
+    std::uint8_t const& major() const { return std::get<0>(*this); }
+    std::uint8_t & major() { return std::get<0>(*this); }
 
-    std::unit8_t const& minor() const { return std::get<1>(*this); }
-    td::unit8_t & minor() { return std::get<1>(*this); }
+    std::uint8_t const& minor() const { return std::get<1>(*this); }
+    std::uint8_t & minor() { return std::get<1>(*this); }
 };
 
 
 struct HttpResponse {
-    std::uint8_t status;
-    std::string status_line;
+    std::uint16_t status;
+    std::vector<char> status_line;
     HttpVersion version;
 
-    // TODO: Headers
+    HttpHeaders headers;
 
     std::vector<char> body;
 };
@@ -38,8 +64,11 @@ public:
     void on_close();
 
     // http11 callbacks
-    bool on_body(http11::http_parser&, std::vector<char>);
-    bool on_message_complete(http11::http_parser&);
+    bool on_h_field(httpp11::http_parser&, std::vector<char>);
+    bool on_h_value(httpp11::http_parser&, std::vector<char>);
+    bool on_body(httpp11::http_parser&, std::vector<char>);
+    bool on_status(httpp11::http_parser&, std::vector<char>);
+    bool on_message_complete(httpp11::http_parser&);
 
     std::function<void(HttpResponse&&)> callback;
 
@@ -48,5 +77,13 @@ private:
     httpp11::unique_http_parser_settings settings;
 
     std::vector<char> body;
+    std::vector<char> status_line;
 
+    enum class HeaderState {FIELD, VALUE};
+    HeaderState headerState = HeaderState::FIELD;
+
+    std::string header_field;
+    std::vector<char> header_value;
+
+    HttpHeaders headers;
 };
--- src/http_response_parser.cpp
+++ src/http_response_parser.cpp
@@ -1,18 +1,18 @@
 #include "http_response_parser.hh"
 
 #include "deferred.h"
-#include "http11.hh"
+#include "httpp11.hh"
 
 #include <functional>
 
-http11::http_data_cb bind_data_fn(
+httpp11::http_data_cb bind_data_fn(
     HttpResponseParser* p,
     bool(HttpResponseParser::* fn)(httpp11::http_parser&, std::vector<char>)
 ) {
     return std::bind(std::mem_fn(fn), p, std::placeholders::_1, std::placeholders::_2);
 }
 
-http11::http_cb bind_fn(
+httpp11::http_cb bind_fn(
         HttpResponseParser* p,
         bool(HttpResponseParser::* fn)(httpp11::http_parser&)
 ) {
@@ -20,11 +20,13 @@ http11::http_cb bind_fn(
 }
 
 HttpResponseParser::HttpResponseParser()
-: parser(httpp11::http_parser_init(httpp11::http_parser_type::http_response),
-  settings(httpp11::http_parser_settings_init()))
+: parser(httpp11::http_parser_init(httpp11::http_parser_type::http_response)), settings(httpp11::http_parser_settings_init())
 {
     // Safe to capture `this` as settings' lifespan is tied to `this`.
-    settings->body = bind_data_fn(this, &HttpResponseParser::on_data);
+    settings->status = bind_data_fn(this, &HttpResponseParser::on_status);
+    settings->header_field = bind_data_fn(this, &HttpResponseParser::on_h_field);
+    settings->header_value = bind_data_fn(this, &HttpResponseParser::on_h_value);
+    settings->body = bind_data_fn(this, &HttpResponseParser::on_body);
     settings->message_complete = bind_fn(this, &HttpResponseParser::on_message_complete);
 
 }
@@ -32,9 +34,11 @@ HttpResponseParser::HttpResponseParser()
 Deferred<> HttpResponseParser::on_data(std::vector<char>&& data) {
     httpp11::http_parser_execute(*parser, *settings, data);
     // TODO: Exception.
+
+    return Deferred<>::make_succeeded();
 }
 
-void HttpResponseParser::on_error(typename Error const& err, bool fatal) {
+void HttpResponseParser::on_error(Error const& err, bool fatal) {
     // TODO
     if (fatal) {
         on_close();
@@ -48,13 +52,52 @@ void HttpResponseParser::on_close() {
     httpp11::http_parser_execute(*parser, *settings, std::vector<char>());
 }
 
-bool HttpResponseParser::on_body(http11::http_parser&, std::vector<char>) {
-    std::copy(data.begin(), data.end(), std::back_inserter(body));
+bool HttpResponseParser::on_h_field(httpp11::http_parser&, std::vector<char> data) {
+    if (headerState == HeaderState::FIELD) {
+        std::copy(data.begin(), data.end(), std::back_inserter(header_field));
+    } else {
+        // We have a field, value header pair. Insert it into headers
+        headers.insert({std::move(header_field), std::move(header_value)});
+
+        headerState = HeaderState::FIELD;
+        header_field = std::string(data.begin(), data.end());
+    }
+
+    return 0;
 }
 
-bool HttpResponseParser::on_message_complete(http11::http_parser&) {
-    HttpResponse response{status, status_line, version, std::move(body)};
+bool HttpResponseParser::on_h_value(httpp11::http_parser&, std::vector<char> data) {
+    if (headerState == HeaderState::VALUE) {
+        std::copy(data.begin(), data.end(), std::back_inserter(header_value));
+    } else {
+        headerState = HeaderState::VALUE;
+        header_value = std::move(data);
+    }
+
+    return 0;
+}
+
+bool HttpResponseParser::on_body(httpp11::http_parser&, std::vector<char> data) {
+    std::copy(data.begin(), data.end(), std::back_inserter(body));
+    return 0;
+}
+
+bool HttpResponseParser::on_status(httpp11::http_parser&, std::vector<char> data) {
+    std::copy(data.begin(), data.end(), std::back_inserter(status_line));
+    return 0;
+}
+
+bool HttpResponseParser::on_message_complete(httpp11::http_parser& p) {
+    HttpResponse response{
+            static_cast<std::uint16_t>(p.Get().status_code), // We know this is fine from http_parser.h
+            status_line,
+            HttpVersion(p.Get().http_major, p.Get().http_minor),
+            std::move(headers),
+            std::move(body)
+    };
 
     // TODO
     callback(std::move(response));
+
+    return 0;
 }
